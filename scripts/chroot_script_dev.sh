cc55c6b71f243438a34e117c9ea72d10752148ba
Jon Ludlam <jonathan.ludlam@citrix.com>
2016-02-27 11:29:04 +0000

    Remove redundant git installation
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- scripts/chroot_script_dev.sh
+++ scripts/chroot_script_dev.sh
@@ -13,14 +13,3 @@ EOF
 
 chkconfig network on
 
-CENTOS_VERSION=`rpm -q centos-release --qf %{version}`
-
-if [ $CENTOS_VERSION -eq '5' ]; then
-   rpm -Uvh http://dl.fedoraproject.org/pub/epel/5/i386/epel-release-5-4.noarch.rpm
-   yum install -y --enablerepo=base --disablerepo=citrix git-core
-else 
-   yum install -y --enablerepo=base --disablerepo=citrix git
-fi
-cd /home/vagrant
-
-git clone git://github.com/jonludlam/vagrant-xenserver-scripts
